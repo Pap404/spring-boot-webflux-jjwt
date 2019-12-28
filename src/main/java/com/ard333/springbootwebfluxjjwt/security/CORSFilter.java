9fa6cab46b20285219c4a8a92318baad055a76e2
Pap404 <el23022002@gmail.com>
2019-12-28 17:53:22 +0300

    Add CrossOrigin

--- src/main/java/com/ard333/springbootwebfluxjjwt/security/CORSFilter.java
+++ src/main/java/com/ard333/springbootwebfluxjjwt/security/CORSFilter.java
@@ -1,6 +1,7 @@
 package com.ard333.springbootwebfluxjjwt.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
@@ -11,6 +12,8 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
  */
 @Configuration
 @EnableWebFlux
+@CrossOrigin(origins = "http://localhost:3000")
+
 public class CORSFilter implements WebFluxConfigurer {
 
 	@Override
